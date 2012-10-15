99bb79ebb305bd634eb30e8b4a4225960581e07c
Alexei Karpenko <alexei@karpenko.ca>
2012-10-14 18:50:42 -0700

    Fixes and support Firefox

--- lib/compass/sass_extensions/functions/retina_images.rb
+++ lib/compass/sass_extensions/functions/retina_images.rb
@@ -1,13 +1,12 @@
 module Compass::SassExtensions::Functions::RetinaImages
   def file_exists(filename)
-    !Rails.application.assets.find_asset(filename.value).nil?
+    Sass::Script::Bool.new !Rails.application.assets.find_asset(filename.value).nil?
   end
 
   def retina_filename(filename)
     filename = filename.value
     extension = File.extname(filename)
-    filename.chomp! extension
-    Sass::Script::String.new "#{filename}@2x#{extension}"
+    Sass::Script::String.new "#{filename.chomp(extension)}@2x#{extension}"
   end
 end
 
